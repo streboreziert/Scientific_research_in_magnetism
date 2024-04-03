b43365e5aaf1f7b9b813d2a1348d68e4fe25a98a
Roberts Treize <93783050+streboreziert@users.noreply.github.com>
2024-04-03 16:48:38 +0300

    Update and rename Calculator.py to Angular_momentums.py

---
+++ Angular_momentums.py
@@ -0,0 +1,13 @@
+π=3.14
+q=1260 # Density of glycerin
+r=0.01 # Radiuss of ball
+η=0.13 # Viscosity
+M=0.02
+B=0.001 
+y = 8*pi*n*(r**3)
+
+WI=(15*η)/(q*r*r)
+WC=(M*B)/y
+
+print("WI:"WI)
+print("WC:"WC)
